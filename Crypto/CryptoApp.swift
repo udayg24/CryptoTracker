8b4b18a03da53241c3a35f740b324bceaaf130fe
udayg24 <udaygajera116@gmail.com>
2024-01-24 12:37:31 +0530

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Uday Gajera on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
